2347f64c39c996e9ea0cc357f85fad968ff0282b
Ivo Roberto Batistela <byivo@byivo-personal>
2016-02-29 04:21:18 -0300

    mensagens adicionadas

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,34 @@
 var Helper = {
 	isCarroIgual: function(carro, outro) {
 		return carro.placa.toLowerCase() == outro.placa.toLowerCase();
-	}
+	},
+
+	showMensagemConfirmacao: function(mensagem, acao) {
+		swal({
+				title: "Você tem certeza?",   
+				text: mensagem,   
+				type: "warning",   
+				showCancelButton: true,
+				calcelButtonText: 'Cancelar',
+				confirmButtonText: "Continuar",   
+				closeOnConfirm: true 
+			}, function(){   
+				if(acao) {
+					acao();
+				}
+			});
+	},
+
+	showMensagemErro: function(mensagem) {
+		swal({
+				title: "Ops!",   
+				text: mensagem,
+				type: "error",   
+				confirmButtonText: "Fechar"
+		});
+	},
+
+	showMensagemOk: {}
 };
 
 angular.module('conta-azul', ['ngRoute', 'angularUtils.directives.dirPagination']).config(['$routeProvider', function($routeProvider) {
