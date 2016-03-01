14d4768064caf0b171f6245a11f4e8c990cc2b1b
Ivo Roberto Batistela <byivo@byivo-personal>
2016-03-01 08:12:36 -0300

    correcao no campo valor dos carrros pré-inscritos

--- public/js/carros/services/Service.js
+++ public/js/carros/services/Service.js
@@ -6,88 +6,24 @@
 		'imagem' : null, 
 		'marca' : 'Volkswagem', 
 		'modelo' : 'Gol', 
-		'placa' : 'FFF5498', 
-		'valor' : '20000' 
+		'placa' : 'FFF-5498', 
+		'valor' : 20000 
 	}, 
 	{ 
 		'combustivel' : 'Gasolina', 
 		'imagem' : null, 
 		'marca' : 'Volkswagem',     
 		'modelo' : 'Fox', 
-		'placa' : 'FOX4125', 
-		'valor' : '20000' 
+		'placa' : 'FOX-4125', 
+		'valor' : 20000
 	}, 
 	{
 		'combustivel' : 'Alcool', 
-		'imagem' : 'https://lh4.googleusercontent.com/_AhcQKHf7rM/AAAAAAAAAAI/AAAAAAAAAAA/QMpqL4NYaE/s48ckno/photo.jpg', 
+		'imagem' : 'https://upload.wikimedia.org/wikipedia/commons/6/67/Fusca_1500_frente.jpg', 
 		'marca' : 'Volkswagen', 
 		'modelo' : 'Fusca', 
-		'placa' : 'PAI4121', 
-		'valor' : '20000' 
-	},
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'Gol', 
-		'placa' : 'FFF5410', 
-		'valor' : '20000' 
-	},
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'A1', 
-		'placa' : 'CCC', 
-		'valor' : '20000' 
-	}, 
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'A2', 
-		'placa' : 'DDD', 
-		'valor' : '20000' 
-	}, 
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'A3', 
-		'placa' : 'EEE', 
-		'valor' : '20000' 
-	}, 
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'A4', 
-		'placa' : 'FFF', 
-		'valor' : '20000' 
-	}, 
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'A5', 
-		'placa' : 'GGG', 
-		'valor' : '20000' 
-	}, 
-	{ 
-		'combustivel' : 'Flex', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem', 
-		'modelo' : 'A6', 
-		'placa' : 'HHH', 
-		'valor' : '20000' 
-	}, 
-	{ 
-		'combustivel' : 'Flexs', 
-		'imagem' : null, 
-		'marca' : 'Volkswagem',
-		'modelo' : 'A7', 
-		'placa' : 'III',
-		'valor' : '20000' 
+		'placa' : 'PAI-4121', 
+		'valor' : 20000
 	}
 	];
 
